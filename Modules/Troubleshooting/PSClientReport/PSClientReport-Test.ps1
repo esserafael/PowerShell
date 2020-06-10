f4be7cd710283c58a25632dac94ee72972278454
esserafael <esserafael@gmail.com>
2020-06-09 22:47:52 -0300

    Wtf.

--- Modules/Troubleshooting/PSClientReport/PSClientReport-Test.ps1
+++ Modules/Troubleshooting/PSClientReport/PSClientReport-Test.ps1
@@ -1,4 +1,50 @@
-﻿function Get-ClientInfo
+﻿
+function ConvertTo-HtmlReport
+{
+	<#
+	.SYNOPSIS
+		Gets Active Directory Replication status and results.
+
+	.DESCRIPTION
+		Gets Active Directory Replication status and results.
+	#>
+
+	[CmdletBinding()]
+	[OutputType([String])]
+	Param
+	(
+		[String]$Path = "C:\Users\Feustel\Documents\HtmlResult.html"
+	)
+
+	begin
+	{
+
+	}
+
+	process
+	{
+		[String]$HtmlText = Get-Content -Path (Split-Path -Path $MyInvocation.ScriptName | Join-Path -ChildPath "Templates\ReportTemplateWrapper.html")
+		
+		# Insert PS_INCLUDEs
+		[Regex]$Regex = "{{PS_INCLUDE:(.*?\.html)}}"
+				if($HtmlText -match $Regex)
+				{
+					($HtmlText | Select-String -Pattern $Regex -AllMatches).Matches | ForEach-Object {
+						$HtmlText = $HtmlText -replace $_.Value, (Get-Content -Path (Split-Path -Path $MyInvocation.ScriptName | Join-Path -ChildPath "Templates\$($_.Groups[1].Value)"))
+					}
+				}
+		
+		#Set-Content -Path $Path -Value $HtmlText
+		Return $HtmlText
+	}
+
+	end
+	{
+
+	}
+}
+
+function Get-ClientInfo
 {
 	<#
 	.SYNOPSIS
@@ -22,7 +68,10 @@
 
 	process
 	{
+		$EnclosureInfo = Get-CimInstance -ClassName Win32_SystemEnclosure
 		$ComputerSystemInfo = Get-CimInstance -ClassName CIM_ComputerSystem
+		$ProcessorInfo = Get-CimInstance -ClassName Win32_Processor
+		$PhysicalMemInfo = Get-CimInstance -class Win32_PhysicalMemory
 
 		#Return [PSCustomObject]([ordered]@{}
 	}
@@ -79,4 +128,4 @@ function Test-InternetConnection
 
 . .\Modules\Troubleshooting\ClientReport\ClientReport-Config.ps1
 
-Test-InternetConnection
+ConvertTo-HtmlReport
