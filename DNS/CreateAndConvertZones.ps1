cd6f9c56a54962c1eccd6c1466dc84383055a04f
Rafael Alexandre Feustel Gustmann <rafael.gustmann@uniasselvi.com.br>
2021-03-16 12:37:07 -0300

    DNS scripts

---
+++ DNS/CreateAndConvertZones.ps1
@@ -0,0 +1,11 @@
+﻿Get-ChildItem C:\Zonas\ | ForEach-Object {
+	$ZoneName = $_.Name
+	Add-DnsServerSecondaryZone -MasterServers <IP> -Name $ZoneName -ZoneFile "$($ZoneName).dns"
+}
+
+Get-ChildItem C:\Zonas\ | ForEach-Object {
+	$ZoneName = $_.Name
+	ConvertTo-DnsServerPrimaryZone $ZoneName -PassThru -Verbose -ZoneFile "$($ZoneName).dns" -Force
+	ConvertTo-DnsServerPrimaryZone $ZoneName -PassThru -Verbose -ReplicationScope Domain -Force
+	Get-DnsServerResourceRecord -ZoneName $ZoneName -RRType NS | Where-Object {$_.RecordData.NameServer -like "ns*"} | Remove-DnsServerResourceRecord -ZoneName $ZoneName -Force
+}
